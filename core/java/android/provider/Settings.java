24f11e686b6aa7060a660bcf90ec5a60916e9ed8
a.derendyaev <a.derendyaev@magdv.com>
2018-12-17 16:28:44 +0700

    Extended audio panel [1/2]
    Change-Id: Ieb85137ee73f7ed032b9bbdd4087be2c72a4d1f5
    Signed-off-by: a.derendyaev <a.derendyaev@magdv.com>
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4507,6 +4507,24 @@ public final class Settings {
         private static final Validator DISPLAY_CUTOUT_HIDDEN_VALIDATOR = new
                 SettingsValidators.InclusiveIntegerRangeValidator(0, 1);
 
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_MEDIA = "audio_panel_view_media";
+
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_RINGER = "audio_panel_view_ringer";
+
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_NOTIFICATION = "audio_panel_view_notification";
+
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_ALARM = "audio_panel_view_alarm";
+
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_VOICE = "audio_panel_view_voice";
+
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_BT_SCO = "audio_panel_view_bt_sco";
+
         /**
          * whether to enable or disable vibration on succesful fingerprint auth
          *
