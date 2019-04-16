venkysai/android_frameworks_base-3

[SQUASHED] Carrier label and Status Bar clock Font and Color cutoms.. [1/2] Carrier Label font styles [1/2]
Statusbar Clock Font styles [1/2]

Add Clock size & font style.

Port to Pie - SuperDroidBond
Clock fonts and size - @temasek
Port to Oreo by @varund7726

Add Statusbar Clock and Carrier Label color [1/2]

packages/SystemUI/src/com/android/systemui/stag/carrierlabel/CarrierLabel.java
@@ -13,14 +13,16 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- package com.android.systemui.stag.carrierlabel;
- import android.content.BroadcastReceiver;
+package com.android.systemui.stag.carrierlabel;
+import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.database.ContentObserver;
+import android.graphics.Color;
 import android.graphics.Rect;
+import android.graphics.Typeface;
 import android.os.Handler;
 import android.os.UserHandle;
 import android.provider.Settings;
@@ -30,31 +32,83 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
- import com.android.internal.util.stag.StagUtils;
+import com.android.internal.util.stag.StagUtils;
 import com.android.internal.telephony.TelephonyIntents;
- import com.android.systemui.Dependency;
+import com.android.systemui.Dependency;
 import com.android.systemui.stag.carrierlabel.SpnOverride;
 import com.android.systemui.statusbar.policy.DarkIconDispatcher;
 import com.android.systemui.statusbar.policy.DarkIconDispatcher.DarkReceiver;
- import java.text.SimpleDateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.TimeZone;
- import com.android.systemui.R;
- public class CarrierLabel extends TextView implements DarkReceiver {
-     private Context mContext;
+import com.android.systemui.R;
+public class CarrierLabel extends TextView implements DarkReceiver {
+    private Context mContext;
     private boolean mAttached;
     private static boolean isCN;
-     public CarrierLabel(Context context) {
+    private int mCarrierFontSize = 14;
+    private int mCarrierColor = 0xffffffff;
+    private int mTintColor = Color.WHITE;
+
+    private int mCarrierLabelFontStyle = FONT_NORMAL;
+    public static final int FONT_NORMAL = 0;
+    public static final int FONT_ITALIC = 1;
+    public static final int FONT_BOLD = 2;
+    public static final int FONT_BOLD_ITALIC = 3;
+    public static final int FONT_LIGHT = 4;
+    public static final int FONT_LIGHT_ITALIC = 5;
+    public static final int FONT_THIN = 6;
+    public static final int FONT_THIN_ITALIC = 7;
+    public static final int FONT_CONDENSED = 8;
+    public static final int FONT_CONDENSED_ITALIC = 9;
+    public static final int FONT_CONDENSED_LIGHT = 10;
+    public static final int FONT_CONDENSED_LIGHT_ITALIC = 11;
+    public static final int FONT_CONDENSED_BOLD = 12;
+    public static final int FONT_CONDENSED_BOLD_ITALIC = 13;
+    public static final int FONT_MEDIUM = 14;
+    public static final int FONT_MEDIUM_ITALIC = 15;
+    public static final int FONT_BLACK = 16;
+    public static final int FONT_BLACK_ITALIC = 17;
+     Handler mHandler;
+     class SettingsObserver extends ContentObserver {
+        SettingsObserver(Handler handler) {
+            super(handler);
+        }
+         void observe() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(Settings.System
+                    .getUriFor(Settings.System.STATUS_BAR_CARRIER_COLOR), false, this);
+            resolver.registerContentObserver(Settings.System
+                    .getUriFor(Settings.System.STATUS_BAR_CARRIER_FONT_SIZE), false, this);
+            resolver.registerContentObserver(Settings.System
+                    .getUriFor(Settings.System.STATUS_BAR_CARRIER_FONT_STYLE), false, this);
+        }
+         @Override
+        public void onChange(boolean selfChange) {
+	        updateColor();
+	        updateSize();
+	        updateStyle();
+        }
+    }
+
+    public CarrierLabel(Context context) {
         this(context, null);
     }
-     public CarrierLabel(Context context, AttributeSet attrs) {
+    public CarrierLabel(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
     }
-     public CarrierLabel(Context context, AttributeSet attrs, int defStyle) {
+    public CarrierLabel(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         mContext = context;
         updateNetworkName(true, null, false, null);
+        mHandler = new Handler();
+        SettingsObserver settingsObserver = new SettingsObserver(mHandler);
+        settingsObserver.observe();
+        updateColor();
+        updateSize();
+        updateStyle();
     }
+
      @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
@@ -78,8 +132,14 @@ import java.util.TimeZone;
     }
      @Override
     public void onDarkChanged(Rect area, float darkIntensity, int tint) {
-        setTextColor(DarkIconDispatcher.getTint(area, this, tint));
+        mTintColor = DarkIconDispatcher.getTint(area, this, tint);
+        if (mCarrierColor == 0xFFFFFFFF) {
+            setTextColor(mTintColor);
+        } else {
+            setTextColor(mCarrierColor);
+        }
     }
+
      private final BroadcastReceiver mIntentReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -132,4 +192,104 @@ import java.util.TimeZone;
         }
         return operatorName;
     }
+
+   public void getFontStyle(int font) {
+        switch (font) {
+            case FONT_NORMAL:
+            default:
+                setTypeface(Typeface.create("sans-serif",
+                    Typeface.NORMAL));
+                break;
+            case FONT_ITALIC:
+                setTypeface(Typeface.create("sans-serif",
+                    Typeface.ITALIC));
+                break;
+            case FONT_BOLD:
+                setTypeface(Typeface.create("sans-serif",
+                    Typeface.BOLD));
+                break;
+            case FONT_BOLD_ITALIC:
+                setTypeface(Typeface.create("sans-serif",
+                    Typeface.BOLD_ITALIC));
+                break;
+            case FONT_LIGHT:
+                setTypeface(Typeface.create("sans-serif-light",
+                    Typeface.NORMAL));
+                break;
+            case FONT_LIGHT_ITALIC:
+                setTypeface(Typeface.create("sans-serif-light",
+                    Typeface.ITALIC));
+                break;
+            case FONT_THIN:
+                setTypeface(Typeface.create("sans-serif-thin",
+                    Typeface.NORMAL));
+                break;
+            case FONT_THIN_ITALIC:
+                setTypeface(Typeface.create("sans-serif-thin",
+                    Typeface.ITALIC));
+                break;
+            case FONT_CONDENSED:
+                setTypeface(Typeface.create("sans-serif-condensed",
+                    Typeface.NORMAL));
+                break;
+            case FONT_CONDENSED_ITALIC:
+                setTypeface(Typeface.create("sans-serif-condensed",
+                    Typeface.ITALIC));
+                break;
+            case FONT_CONDENSED_LIGHT:
+                setTypeface(Typeface.create("sans-serif-condensed-light",
+                    Typeface.NORMAL));
+                break;
+            case FONT_CONDENSED_LIGHT_ITALIC:
+                setTypeface(Typeface.create("sans-serif-condensed-light",
+                    Typeface.ITALIC));
+                break;
+            case FONT_CONDENSED_BOLD:
+                setTypeface(Typeface.create("sans-serif-condensed",
+                    Typeface.BOLD));
+                break;
+            case FONT_CONDENSED_BOLD_ITALIC:
+                setTypeface(Typeface.create("sans-serif-condensed",
+                    Typeface.BOLD_ITALIC));
+                break;
+            case FONT_MEDIUM:
+                setTypeface(Typeface.create("sans-serif-medium",
+                    Typeface.NORMAL));
+                break;
+            case FONT_MEDIUM_ITALIC:
+                setTypeface(Typeface.create("sans-serif-medium",
+                    Typeface.ITALIC));
+                break;
+            case FONT_BLACK:
+                setTypeface(Typeface.create("sans-serif-black",
+                    Typeface.NORMAL));
+                break;
+            case FONT_BLACK_ITALIC:
+                setTypeface(Typeface.create("sans-serif-black",
+                    Typeface.ITALIC));
+                break;
+        }
+    }
+
+    private void updateColor() {
+        mCarrierColor = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_CARRIER_COLOR, 0xffffffff);
+        if (mCarrierColor == 0xFFFFFFFF) {
+            setTextColor(mTintColor);
+        } else {
+            setTextColor(mCarrierColor);
+        }
+    }
+
+     private void updateSize() {
+        mCarrierFontSize = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_CARRIER_FONT_SIZE, 14);
+        setTextSize(mCarrierFontSize);
+    }
+     private void updateStyle() {
+        mCarrierLabelFontStyle = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_CARRIER_FONT_STYLE, FONT_NORMAL);
+        getFontStyle(mCarrierLabelFontStyle);
+    }
+
 }

ctosp/frameworks_base--archive

Change-Id: I2db71bbad90d35ba54046882f53917f16bfbebcd

packages/SystemUI/src/com/android/systemui/stag/carrierlabel/CarrierLabel.java
@@ -13,14 +13,16 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- package com.android.systemui.stag.carrierlabel;
- import android.content.BroadcastReceiver;
+package com.android.systemui.stag.carrierlabel;
+import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.database.ContentObserver;
+import android.graphics.Color;
 import android.graphics.Rect;
+import android.graphics.Typeface;
 import android.os.Handler;
 import android.os.UserHandle;
 import android.provider.Settings;
@@ -30,31 +32,97 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
- import com.android.internal.util.pixys.PixysUtils;
+import com.android.internal.util.pixys.PixysUtils;
 import com.android.internal.telephony.TelephonyIntents;
- import com.android.systemui.Dependency;
+import com.android.systemui.Dependency;
 import com.android.systemui.stag.carrierlabel.SpnOverride;
 import com.android.systemui.statusbar.policy.DarkIconDispatcher;
 import com.android.systemui.statusbar.policy.DarkIconDispatcher.DarkReceiver;
- import java.text.SimpleDateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.TimeZone;
- import com.android.systemui.R;
- public class CarrierLabel extends TextView implements DarkReceiver {
-     private Context mContext;
+import com.android.systemui.R;
+public class CarrierLabel extends TextView implements DarkReceiver {
+    private Context mContext;
     private boolean mAttached;
     private static boolean isCN;
-     public CarrierLabel(Context context) {
+    private int mCarrierFontSize = 14;
+    private int mCarrierColor = 0xffffffff;
+    private int mTintColor = Color.WHITE;
+
+    private int mCarrierLabelFontStyle = FONT_NORMAL;
+    public static final int FONT_NORMAL = 0;
+    public static final int FONT_ITALIC = 1;
+    public static final int FONT_BOLD = 2;
+    public static final int FONT_BOLD_ITALIC = 3;
+    public static final int FONT_LIGHT = 4;
+    public static final int FONT_LIGHT_ITALIC = 5;
+    public static final int FONT_THIN = 6;
+    public static final int FONT_THIN_ITALIC = 7;
+    public static final int FONT_CONDENSED = 8;
+    public static final int FONT_CONDENSED_ITALIC = 9;
+    public static final int FONT_CONDENSED_LIGHT = 10;
+    public static final int FONT_CONDENSED_LIGHT_ITALIC = 11;
+    public static final int FONT_CONDENSED_BOLD = 12;
+    public static final int FONT_CONDENSED_BOLD_ITALIC = 13;
+    public static final int FONT_MEDIUM = 14;
+    public static final int FONT_MEDIUM_ITALIC = 15;
+    public static final int FONT_BLACK = 16;
+    public static final int FONT_BLACK_ITALIC = 17;
+    public static final int FONT_DANCINGSCRIPT = 18;
+    public static final int FONT_DANCINGSCRIPT_BOLD = 19;
+    public static final int FONT_COMINGSOON = 20;
+    public static final int FONT_NOTOSERIF = 21;
+    public static final int FONT_NOTOSERIF_ITALIC = 22;
+    public static final int FONT_NOTOSERIF_BOLD = 23;
+    public static final int FONT_NOTOSERIF_BOLD_ITALIC = 24;
+    public static final int FONT_GOBOLD = 25;
+    public static final int FONT_ROADRAGE = 26;
+    public static final int FONT_SNOWSTORM = 27;
+    public static final int FONT_GOOGLESANS = 28;
+    public static final int FONT_NEONEON = 29;
+    public static final int FONT_THEMEABLE = 30;
+    public static final int FONT_SAMSUNG = 31;
+     Handler mHandler;
+     class SettingsObserver extends ContentObserver {
+        SettingsObserver(Handler handler) {
+            super(handler);
+        }
+         void observe() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(Settings.System
+                    .getUriFor(Settings.System.STATUS_BAR_CARRIER_COLOR), false, this);
+            resolver.registerContentObserver(Settings.System
+                    .getUriFor(Settings.System.STATUS_BAR_CARRIER_FONT_SIZE), false, this);
+            resolver.registerContentObserver(Settings.System
+                    .getUriFor(Settings.System.STATUS_BAR_CARRIER_FONT_STYLE), false, this);
+        }
+         @Override
+        public void onChange(boolean selfChange) {
+	        updateColor();
+	        updateSize();
+	        updateStyle();
+        }
+    }
+
+    public CarrierLabel(Context context) {
         this(context, null);
     }
-     public CarrierLabel(Context context, AttributeSet attrs) {
+    public CarrierLabel(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
     }
-     public CarrierLabel(Context context, AttributeSet attrs, int defStyle) {
+    public CarrierLabel(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         mContext = context;
         updateNetworkName(true, null, false, null);
+        mHandler = new Handler();
+        SettingsObserver settingsObserver = new SettingsObserver(mHandler);
+        settingsObserver.observe();
+        updateColor();
+        updateSize();
+        updateStyle();
     }
+
      @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
@@ -78,8 +146,14 @@ import java.util.TimeZone;
     }
      @Override
     public void onDarkChanged(Rect area, float darkIntensity, int tint) {
-        setTextColor(DarkIconDispatcher.getTint(area, this, tint));
+        mTintColor = DarkIconDispatcher.getTint(area, this, tint);
+        if (mCarrierColor == 0xFFFFFFFF) {
+            setTextColor(mTintColor);
+        } else {
+            setTextColor(mCarrierColor);
+        }
     }
+
      private final BroadcastReceiver mIntentReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -132,4 +206,160 @@ import java.util.TimeZone;
         }
         return operatorName;
     }
+
+   public void getFontStyle(int font) {
+        switch (font) {
+            case FONT_NORMAL:
+            default:
+                setTypeface(Typeface.create("sans-serif",
+                    Typeface.NORMAL));
+                break;
+            case FONT_ITALIC:
+                setTypeface(Typeface.create("sans-serif",
+                    Typeface.ITALIC));
+                break;
+            case FONT_BOLD:
+                setTypeface(Typeface.create("sans-serif",
+                    Typeface.BOLD));
+                break;
+            case FONT_BOLD_ITALIC:
+                setTypeface(Typeface.create("sans-serif",
+                    Typeface.BOLD_ITALIC));
+                break;
+            case FONT_LIGHT:
+                setTypeface(Typeface.create("sans-serif-light",
+                    Typeface.NORMAL));
+                break;
+            case FONT_LIGHT_ITALIC:
+                setTypeface(Typeface.create("sans-serif-light",
+                    Typeface.ITALIC));
+                break;
+            case FONT_THIN:
+                setTypeface(Typeface.create("sans-serif-thin",
+                    Typeface.NORMAL));
+                break;
+            case FONT_THIN_ITALIC:
+                setTypeface(Typeface.create("sans-serif-thin",
+                    Typeface.ITALIC));
+                break;
+            case FONT_CONDENSED:
+                setTypeface(Typeface.create("sans-serif-condensed",
+                    Typeface.NORMAL));
+                break;
+            case FONT_CONDENSED_ITALIC:
+                setTypeface(Typeface.create("sans-serif-condensed",
+                    Typeface.ITALIC));
+                break;
+            case FONT_CONDENSED_LIGHT:
+                setTypeface(Typeface.create("sans-serif-condensed-light",
+                    Typeface.NORMAL));
+                break;
+            case FONT_CONDENSED_LIGHT_ITALIC:
+                setTypeface(Typeface.create("sans-serif-condensed-light",
+                    Typeface.ITALIC));
+                break;
+            case FONT_CONDENSED_BOLD:
+                setTypeface(Typeface.create("sans-serif-condensed",
+                    Typeface.BOLD));
+                break;
+            case FONT_CONDENSED_BOLD_ITALIC:
+                setTypeface(Typeface.create("sans-serif-condensed",
+                    Typeface.BOLD_ITALIC));
+                break;
+            case FONT_MEDIUM:
+                setTypeface(Typeface.create("sans-serif-medium",
+                    Typeface.NORMAL));
+                break;
+            case FONT_MEDIUM_ITALIC:
+                setTypeface(Typeface.create("sans-serif-medium",
+                    Typeface.ITALIC));
+                break;
+            case FONT_BLACK:
+                setTypeface(Typeface.create("sans-serif-black",
+                    Typeface.NORMAL));
+                break;
+            case FONT_BLACK_ITALIC:
+                setTypeface(Typeface.create("sans-serif-black",
+                    Typeface.ITALIC));
+                break;
+            case FONT_DANCINGSCRIPT:
+                setTypeface(Typeface.create("cursive",
+                    Typeface.NORMAL));
+                break;
+            case FONT_DANCINGSCRIPT_BOLD:
+                setTypeface(Typeface.create("cursive",
+                    Typeface.BOLD));
+                break;
+            case FONT_COMINGSOON:
+                setTypeface(Typeface.create("casual",
+                    Typeface.NORMAL));
+                break;
+            case FONT_NOTOSERIF:
+                setTypeface(Typeface.create("serif",
+                    Typeface.NORMAL));
+                break;
+            case FONT_NOTOSERIF_ITALIC:
+                setTypeface(Typeface.create("serif",
+                    Typeface.ITALIC));
+                break;
+            case FONT_NOTOSERIF_BOLD:
+                setTypeface(Typeface.create("serif",
+                    Typeface.BOLD));
+                break;
+            case FONT_NOTOSERIF_BOLD_ITALIC:
+                setTypeface(Typeface.create("serif",
+                    Typeface.BOLD_ITALIC));
+                break;
+            case FONT_GOBOLD:
+                setTypeface(Typeface.create("gobold-light-sys",
+                    Typeface.NORMAL));
+                break;
+            case FONT_ROADRAGE:
+                setTypeface(Typeface.create("roadrage-sys",
+                    Typeface.NORMAL));
+                break;
+            case FONT_SNOWSTORM:
+                setTypeface(Typeface.create("snowstorm-sys",
+                    Typeface.NORMAL));
+                break;
+            case FONT_GOOGLESANS:
+                setTypeface(Typeface.create("googlesans-sys",
+                    Typeface.NORMAL));
+                break;
+            case FONT_NEONEON:
+                setTypeface(Typeface.create("neoneon-sys",
+                    Typeface.NORMAL));
+                break;
+            case FONT_THEMEABLE:
+                setTypeface(Typeface.create("themeable-sys",
+                    Typeface.NORMAL));
+                break;
+            case FONT_SAMSUNG:
+                setTypeface(Typeface.create("samsung-sys",
+                    Typeface.NORMAL));
+                break;
+        }
+    }
+
+    private void updateColor() {
+        mCarrierColor = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_CARRIER_COLOR, 0xffffffff);
+        if (mCarrierColor == 0xFFFFFFFF) {
+            setTextColor(mTintColor);
+        } else {
+            setTextColor(mCarrierColor);
+        }
+    }
+
+     private void updateSize() {
+        mCarrierFontSize = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_CARRIER_FONT_SIZE, 14);
+        setTextSize(mCarrierFontSize);
+    }
+     private void updateStyle() {
+        mCarrierLabelFontStyle = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_CARRIER_FONT_STYLE, FONT_NORMAL);
+        getFontStyle(mCarrierLabelFontStyle);
+    }
+
 }