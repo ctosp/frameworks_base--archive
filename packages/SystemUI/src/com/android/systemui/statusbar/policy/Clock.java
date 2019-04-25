94d12339ad2f2d3e9fcf1c370946f61561bcfbcd
Vamsee Krishnaa <46225107+A2O-G@users.noreply.github.com>
2019-04-25 13:59:37 +0530

    Update Clock.java

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -385,8 +385,8 @@ public class Clock extends TextView implements DemoMode, CommandQueue.Callbacks,
     }
 
     @Override
-<<<<<<< HEAD
-=======
+
+
     public void onTuningChanged(String key, String newValue) {
         if (CLOCK_SECONDS.equals(key)) {
             mShowSeconds = TunerService.parseIntegerSwitch(newValue, false);
@@ -399,7 +399,7 @@ public class Clock extends TextView implements DemoMode, CommandQueue.Callbacks,
     }
 
     @Override
->>>>>>> f8b48465e9407eae03cd169c236f45d17bc1fb27
+
     public void disable(int state1, int state2, boolean animate) {
         boolean clockVisibleByPolicy = (state1 & StatusBarManager.DISABLE_CLOCK) == 0;
         if (clockVisibleByPolicy != mClockVisibleByPolicy) {
